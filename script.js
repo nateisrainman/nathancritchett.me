0bc6acc222083d6196bafc7aad18e9a55f519487
Nathan Critchett <nathancritchett@Nathans-MacBook-Pro.local>
2026-04-03 17:01:34 -0700

    Remove card tilt effect

--- script.js
+++ script.js
@@ -84,29 +84,8 @@ function createCustomCursor() {
   });
 }
 
-// ---- 3D Card Tilt ----
-function initCardTilt() {
-  const cards = document.querySelectorAll('.thesis-card, .work-card, .built-card, .stat-card');
-
-  cards.forEach(card => {
-    card.addEventListener('mousemove', (e) => {
-      const rect = card.getBoundingClientRect();
-      const x = e.clientX - rect.left;
-      const y = e.clientY - rect.top;
-      const centerX = rect.width / 2;
-      const centerY = rect.height / 2;
-
-      const rotateX = ((y - centerY) / centerY) * -3;
-      const rotateY = ((x - centerX) / centerX) * 3;
-
-      card.style.transform = `perspective(800px) rotateX(${rotateX}deg) rotateY(${rotateY}deg) translateY(-4px)`;
-    });
-
-    card.addEventListener('mouseleave', () => {
-      card.style.transform = 'perspective(800px) rotateX(0) rotateY(0) translateY(0)';
-    });
-  });
-}
+// ---- 3D Card Tilt (disabled) ----
+function initCardTilt() {}
 
 // ---- Parallax Scrolling ----
 function initParallax() {
